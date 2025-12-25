44b246798c5482369a2a04a95e9d7dfbb8284e13
sfilippenko <fil_mf@mail.ru>
2025-12-25 21:18:10 +0300

    принятие сообщений

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,13 +1,19 @@
 import {
+  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { SendMessageDto } from './dto/send-message.dto';
+import { UsePipes, ValidationPipe } from '@nestjs/common';
 
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
   constructor(private readonly chatService: ChatService) {}
 
   handleConnection(client: Socket): any {
@@ -16,5 +22,14 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   handleDisconnect(client: Socket): any {
     console.log('client disconnected', client.id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @SubscribeMessage('send')
+  async handleMessage(@MessageBody() sendMessageDto: SendMessageDto) {
+    const message = await this.chatService.sendMessage(sendMessageDto);
+    this.server.emit('messages', message);
+
+    return message;
   }
 }
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { SendMessageDto } from './dto/send-message.dto';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async sendMessage(sendMessageDto: SendMessageDto) {
+    return this.prismaService.message.create({
+      data: sendMessageDto,
+    });
+  }
+}
